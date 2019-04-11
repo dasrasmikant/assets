0d0b8e37b38002d144aa63699126effacd8e5985
rasmikant <dasrasmikant@gmail.com>
2019-04-11 15:13:25 +0530

    a

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 console.log("Version is 1.0 and This is a stable version");
 console.log("FIles are modified");
-//Working on new feature
-//working ....
+console.log("Now it is ready to release")
+console.log("released version")
